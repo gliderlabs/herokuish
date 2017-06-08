2cabc72c96197e2c4329d51b257a71d71234baed
Karan Thukral <karanthukral@gmail.com>
2017-06-08 09:50:39 -0400

    Move invalid-proc test to functional tests

--- tests/functional/tests.sh
+++ tests/functional/tests.sh
@@ -32,3 +32,12 @@ T_default-user() {
 	}
 	herokuish-test "test-user" "$(fn-source _test-user)"
 }
+
+T_invalid_proc_process() {
+  local expected_err_msg="Proc entrypoint invalid-proc does not exist. Please check your Procfile"
+  local err_msg=$(herokuish-test "inavlid-proc" "herokuish procfile start invalid-proc")
+  if [[ $err_msg != $expected_err_msg ]]; then
+    echo "procfile-start did not throw error for invalid procfile"
+    exit 1
+  fi
+}
