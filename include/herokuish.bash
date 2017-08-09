bb0ee4410cd2bcda456712efe644991eac18a5e9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-08-09 13:39:30 +0300

    Split `herokuish buildpack test` on several commands to be consistent with buildpacks
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- include/herokuish.bash
+++ include/herokuish.bash
@@ -135,7 +135,9 @@ main() {
 	cmd-export buildpack-build
 	cmd-export buildpack-install
 	cmd-export buildpack-list
+	cmd-export buildpack-test-compile
 	cmd-export buildpack-test
+	cmd-export buildpack-test-with-compile
 
 	cmd-export-ns slug "Manage application slugs"
 	cmd-export slug-import
