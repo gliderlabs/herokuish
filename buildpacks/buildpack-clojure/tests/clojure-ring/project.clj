665b164a0b8e277b4cc43211d2043532ac0f6541
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-08-10 16:11:36 +0300

    Update clojure-ring test app to make lein test work
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- buildpacks/buildpack-clojure/tests/clojure-ring/project.clj
+++ buildpacks/buildpack-clojure/tests/clojure-ring/project.clj
@@ -1,14 +1,12 @@
 (defproject opdemand-helloworld "1.0.0-SNAPSHOT"
   :description "OpDemand Example Application"
-  :dependencies [
-        [org.clojure/clojure "1.2.1"]
-        [compojure "1.0.1"]
-        [ring/ring-core "1.0.0"]
-        [ring/ring-jetty-adapter "1.0.0"]
-        [lein-ring "0.5.4"]
-        ]
-  :dev-dependencies [
-    [org.clojure/clojure-contrib "1.2.0"]
-    ]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.3.4"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [lein-ring "0.8.13"]]
+  :profiles
+  {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}}
   :ring {:handler helloworld.web/handler}
-  :main "helloworld.web")
+  :main helloworld.web)
